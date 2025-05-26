e99c847bf978a9a6c5cba9a3650d6091c6fafe4e
YousifTheKind <yousif.eltayeb@outlook.com>
2025-05-26 11:35:21 +0300

    Call api only once on page load

--- src/routes/Store.jsx
+++ src/routes/Store.jsx
@@ -60,8 +60,12 @@ const useFilms = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
+    // if we already called the api and data is in products no need to call again
+    if (products.length > 0) {
+      setLoading(false);
+      return;
+    }
     const url =
-      // "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
       "https://api.themoviedb.org/3/discover/movie?with_genres=10751,16&sort_by=vote_average.desc&language=en-US&page=1&vote_count.gte=200";
     const options = {
       method: "GET",
@@ -70,33 +74,34 @@ const useFilms = () => {
         Authorization: `Bearer ${API_KEY}`,
       },
     };
-    if (products.length === 0) {
-      fetch(url, options)
-        .then((response) => {
-          if (response.status >= 400) {
-            throw new Error("server error");
-          }
-          return response.json();
-        })
-        .then((json) => {
-          const fetchedFilms = json.results;
-          const newProductsArray = [];
-          fetchedFilms.forEach((film) => {
-            const newProduct = {
-              id: film.id,
-              title: film.title,
-              posterPath: film.poster_path,
-              inCart: true,
-              price: 20,
-              quantity: 0,
-            };
-            newProductsArray.push(newProduct);
-          });
-          setProducts([...newProductsArray]);
-        })
-        .catch((err) => setError(err))
-        .finally(() => setLoading(false));
-    }
+    // if (products.length === 0) {
+    fetch(url, options)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((json) => {
+        console.log("api call");
+        const fetchedFilms = json.results;
+        const newProductsArray = [];
+        fetchedFilms.forEach((film) => {
+          const newProduct = {
+            id: film.id,
+            title: film.title,
+            posterPath: film.poster_path,
+            inCart: true,
+            price: 20,
+            quantity: 0,
+          };
+          newProductsArray.push(newProduct);
+        });
+        setProducts([...newProductsArray]);
+      })
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+    // }
   }, [products.length, setProducts]);
   return { products, error, loading };
 };
