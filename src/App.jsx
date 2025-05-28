9bc410d8fddccfb10ac06e21ad0a5aac998d8b8b
YousifTheKind <yousif.eltayeb@outlook.com>
2025-05-28 17:45:04 +0300

    Deprecated root.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import "./styles/index.css";
+import { useState } from "react";
 import { Routes, Route } from "react-router";
-import Root from "./routes/root";
+import { Outlet } from "react-router";
 import Store from "./routes/Store";
 import Cart from "./routes/Cart";
 import Welcome from "./components/Welcome";
 import ErrorPage from "./components/ErrorPage";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer";
+
 const App = () => {
+  const [products, setProducts] = useState([]);
   return (
-    <Routes>
-      <Route path="/" element={<Root />}>
-        <Route index element={<Welcome />} />
-        <Route path="Store" element={<Store />} />
-        <Route path="Cart" element={<Cart />} />
-      </Route>
-      <Route path="*" element={<ErrorPage />}></Route>
-    </Routes>
+    <>
+      <header>
+        {" "}
+        <Navbar products={products} />
+      </header>
+      <Routes>
+        <Route path="/" element={<Outlet context={[products, setProducts]} />}>
+          <Route index element={<Welcome />} />
+          <Route path="Store" element={<Store />} />
+          <Route path="Cart" element={<Cart />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
+      <Footer />
+    </>
   );
 };
 export default App;
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,19 +1,19 @@
-import { Outlet } from "react-router";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import { useState } from "react";
-const Root = () => {
-  const [products, setProducts] = useState([]);
-  return (
-    <>
-      <header>
-        {" "}
-        <Navbar products={products} />
-      </header>
-      <Outlet context={[products, setProducts]} />
-      <Footer />
-    </>
-  );
-};
-
-export default Root;
+// import { Outlet } from "react-router";
+// import Navbar from "../components/Navbar";
+// import Footer from "../components/Footer";
+// import { useState } from "react";
+// const Root = () => {
+//   const [products, setProducts] = useState([]);
+//   return (
+//     <>
+//       <header>
+//         {" "}
+//         <Navbar products={products} />
+//       </header>
+//       <Outlet context={[products, setProducts]} />
+//       <Footer />
+//     </>
+//   );
+// };
+//
+// export default Root;
