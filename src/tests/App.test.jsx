cee81c860a8955981de090a388b54faa6a16b85f
YousifTheKind <yousif.eltayeb@outlook.com>
2025-05-28 17:16:07 +0300

    test

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,12 +1,16 @@
 import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { Routes, Route, MemoryRouter } from "react-router";
+import { Routes, Route, MemoryRouter, BrowserRouter } from "react-router";
 import userEvent from "@testing-library/user-event";
 import App from "../App.jsx";
 describe("Navigating", () => {
   const user = userEvent.setup();
   it("full app rendering/navigating", async () => {
-    render(<App />, { wrapper: MemoryRouter });
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>,
+    );
   });
   expect(screen.getByRole("main", { name: "Welcome" })).toBeInTheDocument();
 });
